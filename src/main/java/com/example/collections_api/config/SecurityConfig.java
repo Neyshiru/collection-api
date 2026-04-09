5a4a865707269a01af154a920b2dde5211ccd6df
Neyshiru <lucien.lhomme@gmail.com>
2026-04-09 18:48:52 +0200

    add firebase url

--- src/main/java/com/example/collections_api/config/SecurityConfig.java
+++ src/main/java/com/example/collections_api/config/SecurityConfig.java
@@ -23,7 +23,12 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
             .cors(cors -> cors.configurationSource(request -> {
                 var config = new org.springframework.web.cors.CorsConfiguration();
-                config.setAllowedOriginPatterns(List.of("http://localhost:*", "http://127.0.0.1:*"));
+                config.setAllowedOriginPatterns(List.of(
+                	    "http://localhost:*",
+                	    "http://127.0.0.1:*",
+                	    "https://collection-api-58db4.web.app",
+                	    "https://collection-api-58db4.firebaseapp.com"
+                	));
                 config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
                 config.setAllowedHeaders(List.of("*"));
                 config.setAllowCredentials(true);
